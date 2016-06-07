976e2c2267074490d6e22404f607d20838b44416
Manisha Garela <Manisha Garela@192.168.0.12>
2016-06-07 16:26:45 -0700

    FACT COMMIT

---
+++ JavaBasics/src/Basics/basics1.java
@@ -0,0 +1,26 @@
+package Basics;
+public class basics1 {
+	
+	public void factorial(int n)
+	{
+		int fact=1;
+		for (int i=1;i<=n;i++)
+		{
+			fact=fact*i;
+		}
+		
+		
+		
+		System.out.println(fact);
+
+		System.out.println("SECCESS");
+	}
+	
+public static void main(String[] args) {
+	
+	new basics1().factorial(5);
+		
+}
+}
+
+
